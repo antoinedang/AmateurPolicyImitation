633bd136877819ae7191815cb99804ea36e28ed4
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-20 13:37:10 -0400

    add normalization as a parameter in search space for training

--- EXP_train_policies.py
+++ EXP_train_policies.py
@@ -17,8 +17,7 @@ env_configs = [
 starting_parameter_combo = ""  # inclusive
 ending_parameter_combo = ""  # exclusive
 
-normalize = "--normalize"
-normalize = ""  # make empty to disable normalization
+normalization_types = ["--normalize", ""]
 
 
 def main():
@@ -28,28 +27,29 @@ def main():
     for algo in algos:
         for env_config in env_configs:
             for policy_type in policy_types:
-                envid, steps, policy_folder = env_config.split(",")
+                for normalization in normalization_types:
+                    envid, steps, policy_folder = env_config.split(",")
 
-                current_combo = f"{algo},{policy_type},{policy_folder}"
+                    current_combo = f"{algo},{policy_type},{policy_folder}"
 
-                if (
-                    starting_parameter_combo != current_combo
-                    and starting_parameter_combo != ""
-                ):
-                    continue
-                starting_parameter_combo = ""
+                    if (
+                        starting_parameter_combo != current_combo
+                        and starting_parameter_combo != ""
+                    ):
+                        continue
+                    starting_parameter_combo = ""
 
-                if ending_parameter_combo == current_combo:
-                    sys.exit(0)
+                    if ending_parameter_combo == current_combo:
+                        sys.exit(0)
 
-                if policy_type in ["xavier", "orthogonal", "he"]:
-                    init_file = f"policies/{policy_folder}/{policy_type}_{algo}_initialization.pt"
-                else:
-                    init_file = f"policies/{policy_folder}/{policy_type}_{algo}.pt"
+                    if policy_type in ["xavier", "orthogonal", "he"]:
+                        init_file = f"policies/{policy_folder}/{policy_type}_{algo}_initialization.pt"
+                    else:
+                        init_file = f"policies/{policy_folder}/{policy_type}_{algo}.pt"
 
-                command = f"{python_bin} train.py --algo {algo} --env {envid} --n-steps {steps} --init {init_file} {normalize}"
-                print(command)
-                os.system(command)
+                    command = f"{python_bin} train.py --algo {algo} --env {envid} --n-steps {steps} --init {init_file} {normalization}"
+                    print(command)
+                    os.system(command)
 
 
 if __name__ == "__main__":
